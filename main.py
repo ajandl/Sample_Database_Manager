30ff4698cd062a5a5691fe0375a74d126ccb7ea4
Andy Jandl <akjandl@gmail.com>
2017-09-09 13:19:08 -0500

    Minor updates / first commit test by Andy
    Defined main and called it from the if __name__ == '__main__':... block. This is common practice to define main and then call it.
    
    changed USER_DB to lowercase. Capitalised variable names in Python are typically reserved for global variables.

--- main.py
+++ main.py
@@ -33,12 +33,17 @@ def connect_db(db_file):
         print("Error {0}:".format(err.args[0]))
 
 
-if __name__ == "__main__":
-    USER_DB = input("Provide full file path for database or enter 'default': ")
+def main():
 
-    if USER_DB == 'default':
+    user_db = input("Provide full file path for database or enter 'default': ")
+
+    if user_db == 'default':
         connect_db(DEFAULT_DB)
         print("Connected to default DB: DummyDB1.db")
     else:
-        connect_db(USER_DB)
-        print("Connected to user specified DB at: ", USER_DB)
+        connect_db(user_db)
+        print("Connected to user specified DB at: ", user_db)
+
+
+if __name__ == "__main__":
+    main()
