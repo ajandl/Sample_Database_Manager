5cc5d5797c0766673c0a97c4340db3a968c40cf8
ajandl <jandl.wisc@gmail.com>
2017-09-09 13:14:08 -0400

    Update to main

--- main.py
+++ main.py
@@ -4,22 +4,28 @@ Main script for the Sample Database Manager.
 Defines necessary funcitons, classes, and manages program flow as required.
 """
 
-import sys
-import os
 import sqlite3
 
 def connect_db(db_file):
     """
     Creates a connection to the specified database.
 
-    dbFile: Name of database file
+    dbFile [string]: Name of database file
 
-    return: Connection object to the database file or None if unsucessful
+    return [Connection object or None]: Connection object to the database file
+                                        or None if unsucessful
     """
-
+    
     try:
         conn = sqlite3.connect(db_file)
         return conn
     except sqlite3.Error as err:
         print("Error {0}:".format(err.args[0]))
 
+if __name__ == "__main__":
+    USER_DB = input("Provide full file path for database or enter 'default': ")
+
+    if USER_DB == 'default':
+        connect_db(r'D:\Python projects\Sample_Database_Manager\DummyDB\DummyDb1.db')
+    else:
+        connect_db(USER_DB)
